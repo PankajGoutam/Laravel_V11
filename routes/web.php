d5c4a30b5eef53e2692f359dbd68c273536ea5d5
aayan goutam <aayangoutam59@gmail.com>
2024-10-22 19:15:24 +0530

    Removed Query Bulider Files

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\UsersController;
-use App\Http\Controllers\EmpsController;
+
 
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/users',[UsersController::class,'getUsers']);
-
-//Query Builder Usage
-Route::get('/emps',[EmpsController::class,'queries']);
-Route::get('/add_emps',[EmpsController::class,'addEmp']);
-Route::get('/up_emps',[EmpsController::class,'updateEmp']);
-Route::get('/del_emps',[EmpsController::class,'delEmp']);
